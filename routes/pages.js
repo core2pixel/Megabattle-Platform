8829b0400e526a1dd434dad5546ce65aa04c6737
Danl Kladnitskiy <core2pixel@gmail.com>
2020-06-01 19:42:02 +0300

    stream

--- routes/pages.js
+++ routes/pages.js
@@ -149,8 +149,8 @@ router.get('/stream', (req, res) =>{
     let user = req.session.user;
     if(user) {
             console.log(user.user_id + '['+translateFraction(user.fraction)+'] зашёл на стрим.');
-            res.render('stream',  {name:user.username,layout: 'stream', avatar:user.vk_image, fraction: translateFraction(user.fraction), bg: 'stream', link: '5qap5aO4i9A'});  
-        //VazHDDYiPZU
+            res.render('stream',  {name:user.username,layout: 'stream', avatar:user.vk_image, fraction: translateFraction(user.fraction), bg: 'stream', link: 'VazHDDYiPZU'});  
+        //
     }else{
        res.redirect('/notification/noAuth'); 
     }
